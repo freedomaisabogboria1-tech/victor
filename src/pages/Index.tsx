f9ddd27ab8e08badaf24c654af195d5ccc052329
marvellous <marvellousoseh3@gmail.com>
2025-11-07 08:19:14 +0100

    fjf

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Music2 } from "lucide-react";
+import { Music2, Vote } from "lucide-react";
 import podcastImage from "@/assets/podcast-image.jpg";
 import spotifyImage from "@/assets/spotify-image.jpg";
 import googleImage from "@/assets/google-image.jpg";
@@ -106,22 +106,11 @@ const Index = () => {
                 <Button
                   className="w-full sm:w-auto bg-gradient-to-r from-primary to-accent hover:opacity-90 text-white border-0 h-12 px-8"
                 >
-                  <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"/>
-                  </svg>
-                  Vote with Google
+                 <Vote />
+                  Vote Now
                 </Button>
               </a>
-              <a href="#" className="w-full sm:w-auto">
-                <Button
-                  className="w-full sm:w-auto bg-gradient-to-r from-[#1DB954] to-[#1ed760] hover:opacity-90 text-white border-0 h-12 px-8"
-                >
-                  <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z"/>
-                  </svg>
-                  Vote with email
-                </Button>
-              </a>
+              
             </div>
           </div>
 
